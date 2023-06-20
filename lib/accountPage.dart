b97fcc32cc06b3a3362cd75ef4b3afb191c0094c
Zehra Kaygili <zehrakaygili2003@gmail.com>
2023-06-20 19:44:36 +0300

    new file:   assets/images/graphic.png new file:   assets/images/questionicon.png new file:   lib/accountPage.dart new file:   lib/graphicPage.dart

---
+++ lib/accountPage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Account());
+}
+
+class Account extends StatelessWidget {
+  const Account({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const AccountPage(),
+    );
+  }
+}
+
+class AccountPage extends StatelessWidget {
+  const AccountPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Hesabım'),
+      ),
+    );
+  }
+}
